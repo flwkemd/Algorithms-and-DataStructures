33cd1b59040be938c032a668ecc4cc5fd28a520a
flwkemd <flwkemd@naver.com>
2018-02-04 01:18:08 +0100

    implemented Arrays

---
+++ src/Arrays/Arrays.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class Arrays {
+	public static void main(String[] args) {
+		
+		int[] nums = new int[5];
+		int[] nums2 = new int[10];
+		
+		for(int i=1; i<nums.length; i++)
+			nums[i] = i;
+		
+		// O(1) random index
+		int num = nums[0];
+		System.out.println(num);
+		
+		// O(N) "Linear search" --> O(logN) binary trees --> O(1) hashtables
+		for(int i=0; i<nums.length; i++)
+			if(nums[i] == 4)
+				System.out.println("Index found, "+i);
+		
+		nums2[5] = 10;
+	}
+}
