4d939b17e285e840fab2e2b3b8b6166c920c91d3
flwkemd <flwkemd@naver.com>
2018-02-04 03:29:22 +0100

    implemented LinkedList

---
+++ src/LinkedList/LinkedList2.java
@@ -0,0 +1,40 @@
+package LinkedList;
+
+public class LinkedList2 {
+
+	/*
+	 *  It is another implementation of the List interface
+	 *  	~implements List, Queue, Deque interfaces + ...
+	 *  
+	 *   It uses references -> each item has a reference to the next item + store the data itself
+	 *   
+	 *   	34 ->  100 ->  -12 ->  1 single linked list
+	 *   	34 <-> 100 <-> -12 <-> 1 doubly-linked list
+	 *   
+	 *   	- NO RANDOM ACCESS 
+	 *   	- not synchronized
+	 *   	- we can remove items very efficiently / O(1) when removing from the beginning / end
+	 *   	- no sequential access ! if we want to find an tiem we have to iterate through
+	 *   		the list until we find it / O(N)
+	 *   
+	 *   	- so we should use a LinkedList for remove-heavy applications !
+	 */
+
+	public static void main(String[] args) {
+		
+		java.util.List<Integer> list = new java.util.LinkedList<>();
+		
+		list.add(3);
+		list.add(10);
+		list.add(20);
+		
+		list.remove(0); // this is why we like linkedList !
+		System.out.println(list.get(1)); // not that fast
+		
+		System.out.println(list.size());
+		System.out.println(list.isEmpty());
+		
+		for(Integer i : list)
+			System.out.println(i);
+	}
+}
