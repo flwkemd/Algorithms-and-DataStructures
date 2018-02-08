43e83cc0c3c9303fe937716714d7f235c416ace9
flwkemd <flwkemd@naver.com>
2018-02-08 05:24:48 +0100

    implemented Red-Black Trees

---
+++ src/RedBlackTree/App.java
@@ -0,0 +1,14 @@
+package RedBlackTree;
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+		Tree redBlackTree = new RedBlackTree();
+		
+		redBlackTree.insert(10);
+		redBlackTree.insert(20);
+		redBlackTree.insert(30);
+
+	}
+}
